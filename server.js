64a432976c82048e608ff5bf5e407a9d53e3fbbb
SapphireJudah52213 <judahsiegal52213@icloud.com>
2026-06-10 17:42:40 -0400

    Delete server.js

--- server.js
+++
@@ -1,8 +0,0 @@
-const WebSocket = require('ws');
-const wss = new WebSocket.Server({ port: process.env.PORT || 3000 });
-
-wss.on('connection', ws => {
-  ws.send(JSON.stringify({type:'connected'}));
-});
-
-console.log('Server running');
